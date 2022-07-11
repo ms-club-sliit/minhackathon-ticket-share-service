da836b57238279dd5d94796f457b9bc50a66606b
stargazer39 <dehemiweerakkodi2@gmail.com>
2022-07-11 16:05:52 +0530

    Initial commit

---
+++ index.ts
@@ -0,0 +1,64 @@
+import express from "express";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./firebase";
+require('dotenv').config()
+
+const app = express();
+const PORT = process.env.PORT || 4200;
+
+app.set("view engine", "pug");
+
+app.get("/teams/ticket/:id", async (req, res) => {
+    try{
+        let id = req.params.id;
+
+        if(id.trim().length === 0) {
+            res.sendStatus(400);
+            return;
+        }
+        
+        let docRef = doc(db, "teams", id);
+        let docSnap = await getDoc(docRef);
+
+        if(!docSnap.exists()){
+            res.sendStatus(404);
+            return;
+        }
+
+        res.render("ticket", {
+            image_src: docSnap.get("url"),
+            team_name: docSnap.get("team_name"),
+        });
+    }catch(e){
+        res.sendStatus(500);
+    }
+})
+
+app.get("/awareness-session/ticket/:id", async (req, res) => {
+    try{
+        let id = req.params.id;
+
+        if(id.trim().length === 0) {
+            res.sendStatus(400);
+            return;
+        }
+        
+        let docRef = doc(db, "awareness_session", id);
+        let docSnap = await getDoc(docRef);
+
+        if(!docSnap.exists()){
+            res.sendStatus(404);
+            return;
+        }
+
+        res.render("ticket", {
+            image_src: docSnap.get("url"),
+        });
+    }catch(e){
+        res.sendStatus(500);
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+})
